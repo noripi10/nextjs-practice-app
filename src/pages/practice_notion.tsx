980538ebc0840d3fded3b77d9ded7aebdc495826
noripi10 <55979374+noripi10@users.noreply.github.com>
2022-01-13 08:25:16 +0900

    upd

--- src/pages/practice_notion.tsx
+++ src/pages/practice_notion.tsx
@@ -19,7 +19,8 @@ type Blog = {
   description: string
 }
 
-export default function Practice_Notion({ blogs }: { blogs: Blog[] }) {
+export default function Practice_Notion() {
+  // export default function Practice_Notion({ blogs }: { blogs: Blog[] }) {
   const router = useRouter()
 
   return (
@@ -32,11 +33,11 @@ export default function Practice_Notion({ blogs }: { blogs: Blog[] }) {
         <VStack flex='5' h='100vh' maxW='1000px' bgColor='teal.700' p={4} m={4} overflowY='auto'>
           <VStack flex={1}>
             <Text>Notion Api Test</Text>
-            {blogs.map((blog, index) => (
+            {/* {blogs.map((blog, index) => (
               <Text key={index.toString()} flexWrap='nowrap'>
                 {JSON.stringify(blog)}
               </Text>
-            ))}
+            ))} */}
             <Button size='sm' variant='solid' bgColor='green.500' onClick={() => router.back()}>
               back
             </Button>
@@ -51,39 +52,39 @@ export default function Practice_Notion({ blogs }: { blogs: Blog[] }) {
   )
 }
 
-export const getStaticProps = async () => {
-  const table = await notion.databases.query({
-    database_id: '7caef2eb23ba4efa8b999e39419d592f',
-  })
-  // console.info(table)
-  // console.info(table.results[0].properties)
+// export const getStaticProps = async () => {
+//   const table = await notion.databases.query({
+//     database_id: '7caef2eb23ba4efa8b999e39419d592f',
+//   })
+//   // console.info(table)
+//   // console.info(table.results[0].properties)
 
-  const blogs = table.results.map((result) => {
-    const blog = {} as Blog
-    const properties = result.properties
+//   const blogs = table.results.map((result) => {
+//     const blog = {} as Blog
+//     const properties = result.properties
 
-    Object.keys(properties).map((key) => {
-      const property = properties[key]
-      const k = key.toLowerCase()
-      if (property.type === 'title') {
-      }
-      if (property.type === 'rich_text') {
-      }
-      if (property.type === 'checkbox') {
-      }
-      if (property.type === 'people') {
-      }
+//     Object.keys(properties).map((key) => {
+//       const property = properties[key]
+//       const k = key.toLowerCase()
+//       if (property.type === 'title') {
+//       }
+//       if (property.type === 'rich_text') {
+//       }
+//       if (property.type === 'checkbox') {
+//       }
+//       if (property.type === 'people') {
+//       }
 
-      blog[k] = JSON.stringify(property, null, 2)
-    })
+//       blog[k] = JSON.stringify(property, null, 2)
+//     })
 
-    blog.id = result.id
-    return blog
-  })
+//     blog.id = result.id
+//     return blog
+//   })
 
-  return {
-    props: {
-      blogs,
-    },
-  }
-}
+//   return {
+//     props: {
+//       blogs,
+//     },
+//   }
+// }
